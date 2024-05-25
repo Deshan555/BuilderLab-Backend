1fb069cbfdfb5ccad09131880b97a205a5fa2abd
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2024-05-25 09:28:29 +0530

    chore: Add performance monitoring middleware

---
+++ src/middlewares/performanceMonitor.js
@@ -0,0 +1,14 @@
+const performanceMonitor = (req, res, next) => {
+    const startHrTime = process.hrtime();
+  
+    res.on('finish', () => {
+      const elapsedHrTime = process.hrtime(startHrTime);
+      const elapsedTimeInMs = elapsedHrTime[0] * 1000 + elapsedHrTime[1] / 1e6;
+      console.log(`${req.method} ${req.url} [${res.statusCode}] - ${elapsedTimeInMs}ms`);
+    });
+  
+    next();
+  };
+  
+  module.exports = performanceMonitor;
+  
