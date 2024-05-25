74ae56aedd12c026cb6a0ec31d3c78497d13341f
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2024-05-25 09:29:14 +0530

    chore: Update server.js with middleware and route configurations

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./src/config/db');
+const errorHandler = require('./src/middlewares/errorHandler');
+const performanceMonitor = require('./src/middlewares/performanceMonitor');
+const logger = require('./src/config/logger');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(performanceMonitor);
+app.use(require('express-status-monitor')())
+
+connectDB().then((db) => {
+  app.use('/data', require('./src/routes/data')(db));
+  app.use('/checklists', require('./src/routes/checklists')(db));
+});
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  logger.info(`Server is running on port ${PORT}`);
+});
