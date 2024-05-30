63d71f811b5d9f34281aabc9d706d33c1d73ba55
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2024-05-30 22:45:52 +0530

    chore: Update data.js to include version number in inserted data

--- src/routes/data.js
+++ src/routes/data.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { ObjectId } = require('mongodb');
+const { incrementVersion } = require('../utils/versions');
+const { version } = require('winston');
 
 module.exports = (db) => {
   router.get('/', async (req, res) => {
@@ -14,8 +16,18 @@ module.exports = (db) => {
 
   router.post('/', async (req, res) => {
     try {
+      const dataFetch = await db.collection('templates').find({ 'section.sectionID': req.body?.section.sectionID }).toArray();
+      let versionNumber = 'V1.0.0';
+      if (dataFetch.length > 0) {
+        let latestData = dataFetch?.sort((a, b) => {
+          return new Date(b?.createdOn) - new Date(a?.createdOn);
+        });
+        let latestRecord = latestData[0];
+        versionNumber = incrementVersion(latestRecord?.version);
+      }
       const data = {
         ...req.body,
+        version: versionNumber,
         createdOn: new Date().toISOString(),
         updatedOn: new Date().toISOString(),
         uniqueId: Math.floor(Math.random() * 1000000),
@@ -23,6 +35,7 @@ module.exports = (db) => {
       const result = await db.collection('templates').insertOne(data);
       res.json(result);
     } catch (err) {
+      console.log(err);
       res.status(500).json({ error: 'Failed to insert data' });
     }
   });
@@ -63,6 +76,22 @@ module.exports = (db) => {
       res.status(500).json({ error: 'Failed to fetch data' });
     }
   });
+  router.get('/checklist/section/:sectionID', async (req, res) => {
+    try {
+      const data = await db.collection('templates').find({ 'section.sectionID': req.params.sectionID }).toArray();
+let latestData = data.sort((a, b) => {
+    return new Date(b.createdOn) - new Date(a.createdOn);
+});
+
+let latestRecord = latestData[0];
+
+console.log(incrementVersion(latestRecord.version));
+
+      res.json(data);
+    } catch (err) {
+      res.status(500).json({ error: 'Failed to fetch data' });
+    }
+  }); 
 
   return router;
 };
